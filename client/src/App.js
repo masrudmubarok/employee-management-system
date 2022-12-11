97eb7b46616c978f72ddc9c3b6d0b7b09c5f94df
masrudmubarok <masrud.mubarok21@gmail.com>
2022-12-11 22:21:36 +0700

    Add components files

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Home from './components/Home'
+import SideNav from './components/SideNav'
+import Footer from './components/Footer'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Home />
+      <SideNav />
+      <Footer />
     </div>
   );
 }
